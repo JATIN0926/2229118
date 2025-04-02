ed659c9a2ba1a22e9faaf55807a5296bdfa79b87
Jatin Chaudhary <09jatin2005@gmail.com>
2025-04-02 19:13:50 +0530

    added fetchNumbers utility function

---
+++ average_calculator_microservice/src/utils/fetchNumbers.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const API_BASE_URL = process.env.TEST_SERVER_BASE_URL;
+const AUTH_TOKEN = process.env.AUTH_TOKEN; // Store token in .env
+
+const fetchNumbers = async (type) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/${type}`, {
+      headers: {
+        Authorization: `Bearer ${AUTH_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      timeout: 500, 
+    });
+
+    return response.data.numbers || [];
+  } catch (error) {
+    console.error(`Error fetching ${type} numbers:`, error.message);
+    return [];
+  }
+};
+
+export default fetchNumbers;
