57595351267664fdba661b59273b0f877b341b03
Jatin Chaudhary <09jatin2005@gmail.com>
2025-04-02 19:35:18 +0530

    removed unnecessary console logs

--- average_calculator_microservice/src/utils/fetchNumbers.js
+++ average_calculator_microservice/src/utils/fetchNumbers.js
@@ -8,9 +8,6 @@ const AUTH_TOKEN = process.env.AUTH_TOKEN;
 
 const fetchNumbers = async (type) => {
   try {
-    console.log("auth token", AUTH_TOKEN);
-    console.log("type", type);
-    console.log("API_BASE_URL", API_BASE_URL);
     const response = await axios.get(`${API_BASE_URL}/${type}`, {
       headers: {
         Authorization: `Bearer ${AUTH_TOKEN}`,
